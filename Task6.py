adca6967a03a23a096478e5b57c17ecdba2ef923
AleksandraZhukova <72154879+AleksandraZhukova@users.noreply.github.com>
2020-10-13 16:12:29 +0300

    Add files via upload

--- Task6.py
+++ Task6.py
@@ -1,10 +1,1 @@
-def int_func(text):
-    text=text.title()
-    return text
-
-my_text=input("Type word. ")
-print(int_func(my_text))
-
-
-my_huge_text=input("Type text. ")
-print(int_func(my_huge_text))
+#6
