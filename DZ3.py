5e2fd3a6f8693fe27f72adff1454b986f217a634
AleksandraZhukova <72154879+AleksandraZhukova@users.noreply.github.com>
2020-10-13 15:37:27 +0300

    Add files via upload

--- DZ3.py
+++ DZ3.py
@@ -6,7 +6,7 @@ def devide_func(var1, var2):
         return
     else:
         res=var1/var2
-        return (f" Reesult of devide is: {res}")
+        return (f" Result of devide is: {res}")
 
 while True:
     my_var1=float(input("Type first variable"))
